8fad762329b98aefdd23b1aad006d12d19574a67
pouya <pouyakhaleghi82@gmail.com>
2026-05-31 02:00:21 +0330

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+
+const arry = ['pouya', 1,2,3]
+
+
+console.log(arry)
+
+arry[1]='ah'
+
+console.log(arry)
+
+const totalPrice = 100_000
+
+const limit= 1000
+
+console.log(totalPrice>limit)
+
+const name= 'pouya'
+
+
+const selectColor = 'red'
+
+const defaultColor = 'green'
+
+
+console.log(selectColor?selectColor:defaultColor)
+
+console.log(Boolean(2))
+
+let a = 'first'
+let b = 'second'
+
+const c = a
+
+a = b
+b = c
+
+console.log(a)
+console.log(b)
+
+
+const birthYear = 1946
+
+const now = 2026
+
+const yearOfBirth = now-birthYear
+
+const weekOfBirth = yearOfBirth*52
+
+console.log(weekOfBirth*7)
+
