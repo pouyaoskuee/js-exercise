7366c763886cd9b3c115853c65834a6ab34418af
pouya <pouyakhaleghi82@gmail.com>
2026-06-02 20:34:43 +0330

    its commit

--- app.js
+++ app.js
@@ -1,50 +1,118 @@
+//
+// const arry = ['pouya', 1,2,3]
+//
+//
+// console.log(arry)
+//
+// arry[1]='ah'
+//
+// console.log(arry)
+//
+// const totalPrice = 100_000
+//
+// const limit= 1000
+//
+// console.log(totalPrice>limit)
+//
+// const name= 'pouya'
+//
+//
+// const selectColor = 'red'
+//
+// const defaultColor = 'green'
+//
+//
+// console.log(selectColor?selectColor:defaultColor)
+//
+// console.log(Boolean(2))
+//
+// let a = 'first'
+// let b = 'second'
+//
+// const c = a
+//
+// a = b
+// b = c
+//
+// console.log(a)
+// console.log(b)
+//
+//
+// const birthYear = 1946
+//
+// const now = 2026
+//
+// const yearOfBirth = now-birthYear
+//
+// const weekOfBirth = yearOfBirth*52
+//
+// console.log(weekOfBirth*7)
+//
+//
+// const x = 'hello'
+//
+// // console.log(1-x)
+//
+// function ham (){
+//     switch (x){
+//         case x === 'first':
+//             return 'hello'
+//         case x === 'second':
+//
+//     }
+// }
+//
+//
+// let f= 0
+//
+// for (let i=0 ; i <= 10; i++){
+//     f+=3
+//     console.log(f)
+// }
+//
+//
+// let w = 20
+//
+// do{
+//     console.log(w)
+//     w++
+// }while(w<30)
+//
+//
+// const user = {
+//     name:"John",
+//     email:'jhon@hhg.com',
+//     phonNum:48939039
+// }
+//
+// for ( const key of user) {
+//     console.log(key)
+// }
+//
+//
+// const roles = ['admin' , 'teacher' , 'merchant']
+//
+// for (const index of roles) {
+//     console.log(index)
+// }
 
-const arry = ['pouya', 1,2,3]
 
+// let c = 0
+//
+// while (c<10 ){
+//     if (c<5){
+//
+//         if (c===3){
+//             console.log('hello')
+//             continue
+//         }
+//     }
+//     console.log(c)
+//
+// }
 
-console.log(arry)
+function area (radius){
+    console.log(3.14*(radius**2))
+}
 
-arry[1]='ah'
-
-console.log(arry)
-
-const totalPrice = 100_000
-
-const limit= 1000
-
-console.log(totalPrice>limit)
-
-const name= 'pouya'
-
-
-const selectColor = 'red'
-
-const defaultColor = 'green'
-
-
-console.log(selectColor?selectColor:defaultColor)
-
-console.log(Boolean(2))
-
-let a = 'first'
-let b = 'second'
-
-const c = a
-
-a = b
-b = c
-
-console.log(a)
-console.log(b)
-
-
-const birthYear = 1946
-
-const now = 2026
-
-const yearOfBirth = now-birthYear
-
-const weekOfBirth = yearOfBirth*52
-
-console.log(weekOfBirth*7)
-
+area(3)
