59b3a303439db830323c0063156634eb19f032f6
pouya <pouyakhaleghi82@gmail.com>
2026-06-06 21:07:27 +0330

    its commit

--- app.js
+++ app.js
@@ -6,7 +6,8 @@
 //
 // arry[1]='ah'
 //
-// console.log(arry)
+// console.log(ary)
+
 //
 // const totalPrice = 100_000
 //
@@ -111,8 +112,243 @@
 //
 // }
 
-function area (radius){
-    console.log(3.14*(radius**2))
+// function area (radius){
+//     console.log(3.14*(radius**2))
+// }
+//
+// area(3)
+
+// function selectRole (role) {
+//     if (role === 'admin') return 'you are admin'
+//     if (role === 'merchant') return 'you are merchant'
+//     if (role === 'teacher') return 'you are teacher'
+//     return 'you dont chosse role'
+// }
+//
+// console.log(selectRole(''))
+//
+//
+// function selectRoleByCase (role) {
+//     switch (role) {
+//         case 'admin':
+//             return 'you are admin'
+//         case 'teacher':
+//             return 'you are teacher'
+//         case 'merchant':
+//             return 'you are merchant'
+//         default: return  'you dont chosse role'
+//     }
+// }
+//
+// console.log(selectRoleByCase(''))
+
+
+// function witchIsMax ( a, b ){
+//     return a>b? a : b
+//     // if (a>b) return a
+//     // return b
+//
+//
+// }
+
+// console.log(witchIsMax(342525, 44245))
+
+
+// function FizzBuzz (number:number) {
+//     if (number % 3 === 0 && number % 5 === 0 ) return 'its FizzBuzz'
+//     if (number % 3 === 0) return 'its Fizz'
+//     if (number % 5 === 0) return 'its Buzz'
+// }
+//
+// console.log(FizzBuzz(5))
+//
+// console.log(new Date().toISOString())
+
+// function calcDiscountPercent(total:number){
+//     if(total < 100) return  'no discount'
+//     if(total >= 100 && total<110) return 3
+//     if(total >= 110 && total< 125) return 5
+//     if(total >= 125 && total < 140) return 10
+//     return 25
+// }
+//
+//
+// console.log(calcDiscountPercent(200))
+
+// function calcDiscountPercent(total){
+//     if(total < 110) return 'not discount'
+//     if(total >= 140) return 25
+//     const discountStep = 3.3
+//     return Math.round((total - 100)/discountStep);
+// }
+//
+//
+// console.log(calcDiscountPercent(105))
+
+
+// const course = {
+//     title:'react.js',
+//     price:120,
+//     discount: 20,
+//     category: {
+//         id:0,
+//         title:'front',
+//         description:'its front end category'
+//     },
+//     calcPrice: function (){
+//         this.offPrice = (this.price * (1-this.discount/100))
+//
+//
+//     },
+// }
+//
+// // console.log(course.calcPrice())
+//
+// course.calcPrice()
+//
+// console.log(course.offPrice)
+//
+// function factory(title , description , price){
+//     return{
+//         title,
+//         description,
+//         price
+//     }
+// }
+//
+//
+//
+//
+// console.log(factory('react.js', 'its a course' , 100))
+
+
+//
+// function Constructor1 (title , description , price){
+//     this.title = title
+//     this.description = description
+//     this.price = price
+//
+// }
+//
+// const course2= new Constructor1('react.js', 'its a course' , 100)
+//
+// console.log(course2)
+//
+// console.log(course2 instanceof Constructor1)
+//
+// course2.title='pouya'
+//
+// console.log(course2)
+//
+// const test ={
+//     title:'pouya',
+//     description:'pouya is assistant',
+//     price:100
+// }
+//
+// const test2 = test
+//
+//
+// test2.title='nass'
+//
+// console.log(test)
+//
+// console.log(test.hasOwnProperty('title'))
+//
+// console.log(Object.values(test))
+//
+// console.log(Object.entries(test))
+
+//
+//
+// const str = 'pouya'
+//
+// const strObj = new String('pouya')
+//
+//
+//
+// const bool = true
+//
+// const boolObj = new Boolean(bool)
+//
+//
+//
+// const num = 5992
+//
+// const numObj = new Number(num)
+//
+// const array  = [1,2,4]
+//
+// const arrayObj = new Array(array)
+//
+//
+//
+//
+//
+//
+// console.log(typeof str , str)
+//
+// console.log(typeof strObj , strObj)
+//
+// console.log(typeof bool , bool)
+//
+// console.log(typeof boolObj , boolObj)
+//
+// console.log(typeof numObj , numObj)
+
+
+// console.log(typeof arrayObj , arrayObj)
+
+
+// if (NaN){
+//     console.log('true')
+// }else {
+//     console.log('false')
+// }
+//
+//
+//
+//
+// const result = Boolean(NaN) ?? true
+//
+// if (result){
+//     console.log('true')
+// }else {
+//     console.log('false')
+// }
+
+
+const user = {
+    title:'pouya',
+    age:24,
+    fiend:['nas', 'yamete']
 }
 
-area(3)
+const user2 = Object.assign({}, user)
+user2.age=20
+user2.fiend.push('ham')
+console.log(user , user2)
+
+const user3 = JSON.parse(JSON.stringify(user))
+
+user3.fiend.push('dam dam')
+
+console.log(user , user2 , user3)
+
+const course = {
+    title:'react',
+    description:'it is a couurse',
+    category:{
+        id:2,
+        title:'frontend'
+    },
+
+    showWelcome: function (){
+        return `hi Welcome to ${this.title} course`
+    }
+}
+
+const {category: {id , title:categoryTitle} , description} = course
+
+console.log( categoryTitle , course.showWelcome())
+
