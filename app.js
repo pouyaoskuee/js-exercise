a036580c769d6ca3b15dc3951061bca417279787
pouya <pouyakhaleghi82@gmail.com>
2026-06-21 01:21:33 +0330

    its commit

--- app.js
+++ app.js
@@ -318,37 +318,310 @@
 // }
 
 
-const user = {
-    title:'pouya',
-    age:24,
-    fiend:['nas', 'yamete']
-}
+// const user = {
+//     title:'pouya',
+//     age:24,
+//     fiend:['nas', 'yamete']
+// }
+//
+// const user2 = Object.assign({}, user)
+// user2.age=20
+// user2.fiend.push('ham')
+// console.log(user , user2)
+//
+// const user3 = JSON.parse(JSON.stringify(user))
+//
+// user3.fiend.push('dam dam')
+//
+// console.log(user , user2 , user3)
+//
+// const course = {
+//     title:'react',
+//     description:'it is a couurse',
+//     category:{
+//         id:2,
+//         title:'frontend'
+//     },
+//
+//     showWelcome: function (){
+//         return `hi Welcome to ${this.title} course`
+//     }
+// }
+//
+// const {category: {id , title:categoryTitle} , description} = course
+//
+// console.log( categoryTitle , course.showWelcome())
+//
+// const array = ['test' , 3 , 'hello' , 5 ]
+//
+// const [a ,b , c] = array
+//
+// console.log( a , c )
 
-const user2 = Object.assign({}, user)
-user2.age=20
-user2.fiend.push('ham')
-console.log(user , user2)
+// const array = ['frontend' , 'backend']
+//
+// const array2 = [...array , 'fullStack']
+//
+// console.log(array2)
+// console.log(array)
 
-const user3 = JSON.parse(JSON.stringify(user))
+// const array1 = ['react.js', 'vue.js'];
+//
+// const array2 = ['next.js', 'nuxt.js'];
+//
+// array3 = [...array1, ...array2];
+//
+// const [a, b, ...rest] = array3;
+//
+// console.log(a , b , rest)
 
-user3.fiend.push('dam dam')
 
-console.log(user , user2 , user3)
+// function sumWithOff(discount , ...rest){
+//     let totalPrice = 0
+//     for(const total of rest) totalPrice+= total
+//     console.log(totalPrice)
+//     return totalPrice - (totalPrice * discount)
+// }
+//
+// console.log(sumWithOff(0.3,1,2,4,4,5,5,77))
 
-const course = {
-    title:'react',
-    description:'it is a couurse',
-    category:{
-        id:2,
-        title:'frontend'
+
+// console.log(new Set([2,4,5,5]))
+
+// const roles = ['admin', "admin" , 'teacher' , 'owner']
+//
+// // const newRoles = new Set(roles)
+// //
+// // console.log([...newRoles])
+//
+// console.log(roles.join(''))
+//
+// const user = new Map()
+//
+// user.set('admin', 'admin').set(true, 'teacher').set([], 'owner')
+//
+// console.log(user)
+//
+// console.log(user.get(true))
+//
+// // user.clear()
+//
+// console.log(user)
+//
+// const userRoles = new Map([
+//     [{name: 'john'}, 'admin'],
+//     [{name: 'lily'}, 'teacher'],
+//     [{name: 'pouya'}, 'teacher'],
+//     [{name: 'saheb'}, 'teacher'],
+// ])
+//
+// console.log(userRoles)
+//
+//
+// let UniqueRole = new Set()
+//
+// for (let role of userRoles.values()) {
+//     UniqueRole.add(role)
+//
+// }
+//
+// console.log([...UniqueRole])
+
+// function calcDiscount (prices , discount) {
+//     let total = 0
+//     for (let item of prices ) {
+//         total +=  item
+//     }
+//
+//     console.log(total-(total* discount))
+//
+// }
+//
+// calcDiscount([5, 5, 40, 50] , 0.3);
+
+// const nums = [1,2,3,2,4,3,5,1]
+// const uniqNumbers = new Set(nums)
+// const map = new Map()
+//
+//
+// for ( let item of uniqNumbers) {
+//     let value = 0
+//     if (nums.includes(item)) {
+//         value += 1
+//     }
+//     map.set(item, value)
+// }
+//
+// console.log(map)
+
+//
+// const array = [2,4,6,8,10]
+//
+// function sumer (array){
+//     const newArray = []
+//     for (let item of array){
+//         newArray.push(item+2)
+//         console.log(item+2)
+//     }
+//     console.log(newArray)
+// }
+//
+// sumer(array)
+
+// function findStringIndex(str , partOfString){
+//     const result = str.toLowerCase().trim().indexOf(partOfString.toLowerCase())
+//     if (result === -1) return 'trim is not in string'
+//     return result
+// }
+//
+// console.log(findStringIndex('  hello World im am pouya' , 'world'))
+
+// const emails = 'pouyakhaleghi82@gamil.com'
+//
+// function checkEmail(email) {
+//     if (emails=== email.toLowerCase().trim()) return 'this email address is exsist'
+//     return 'welcome to ....'
+// }
+//
+// console.log(checkEmail('pouyakhaleghi82@gamil.com   '))
+
+
+// function capitalizeString (string) {
+//     const lowerString = string.toLowerCase()
+//     const arrayOfString = lowerString.split(' ')
+//     let result = []
+//     for (let item of arrayOfString) {
+//         result.push(item.charAt(0).toUpperCase() + item.slice(1))
+//     }
+//     return result.join(' ')
+// }
+//
+// console.log(capitalizeString('bAmtE'))
+
+// const time = '2:5:35'
+//
+// function timer(time){
+//     const timeArray = time.split(':')
+//     let newTime = []
+//     for (let item of timeArray) {
+//         let padTime = item.padStart(2, '0')
+//         newTime.push(padTime)
+//     }
+//     return newTime.join(':')
+// }
+//
+//
+// console.log(timer(time))
+
+//
+// function maskCredit (number) {
+//     const str = number.toString()
+//     const slicer = str.slice(0,4)
+//      return slicer.padStart(16, '*')
+// }
+//
+// console.log(maskCredit(1234434334345423))
+
+// const str = 'pouya'
+//
+// str.slice(1,3)
+//
+// console.log(str)
+
+//
+// const courses = new Map()
+//
+// courses.set('a', 1)
+//
+// console.log(courses)
+//
+//     // ['next', 'react' , 'vue']
+//
+//
+// courses.forEach((course, index , ham) => {
+//     console.log(course , index)
+// })
+
+
+const users = [
+    {
+        id: 1,
+        name:'pouya',
+        role: 'admin',
+        isActive: true,
+        message:''
     },
-
-    showWelcome: function (){
-        return `hi Welcome to ${this.title} course`
+    {
+        id: 2,
+        name:'fanas',
+        role: 'owner',
+        isActive: false,
+        message:''
+    },
+    {
+        id: 3,
+        name:'darchin',
+        role: 'freelancer',
+        isActive: true,
+        message:''
+    },
+    {
+        id: 4,
+        name:'fesdeq',
+        role: 'freelancer',
+        isActive: false,
+        message:''
     }
+]
+
+
+// const newArray = users.map(user => {
+//     return {...user, message:`hello ${user.name} you are ${user.role} and your account is ${user.isActive?'active':'not active'}`};
+// })
+// console.log(newArray)
+
+// const newUsers = users.filter(user => user.role === "freelancer")
+// console.log(newUsers)
+
+
+// console.log(users.reduce((acc, cur) => {
+//     return acc + cur.id
+// } , 0))
+
+const usersMarks = [9,10,13,18,20,15,7]
+
+
+
+
+function avr (usersMarks , pass=false , shift=0){
+
+    const ShiftArray = usersMarks.map((item)=> item+shift)
+    if (pass){
+        const newArray = ShiftArray.filter(user=> user>= 10)
+        const marks= ShiftArray.reduce((acc, cur , index) => {
+
+            return acc + cur
+        },0)
+        return marks/ newArray.length
+    }
+
+    const marks= ShiftArray.reduce((acc, cur) => acc + cur,0)
+    return (marks/ShiftArray.length).toFixed(2)
+
 }
 
-const {category: {id , title:categoryTitle} , description} = course
+console.log(avr(usersMarks , false ,1 ))
 
-console.log( categoryTitle , course.showWelcome())
+
+
+
+
+
+
+
+
+
+
+
+
 
